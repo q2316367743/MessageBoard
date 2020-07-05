faf5939f417ce8b3141458dd93688c31c265a991
浅梦凉城 <m17762618644@163.com>
2020-07-05 19:21:05 +0800

    init

---
+++ src/main/java/com/qsd/messageboard/MessageBoardApplication.java
@@ -0,0 +1,13 @@
+package com.qsd.messageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBoardApplication.class, args);
+	}
+
+}
